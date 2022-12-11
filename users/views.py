270ddf3712374cd99bcccd902611860a3a77ea0c
roy-west <110616550+roy-west@users.noreply.github.com>
2022-12-11 23:18:21 +0800

    verification deleted

--- users/views.py
+++ users/views.py
@@ -4,7 +4,6 @@ from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from .serializers import RegisterSerializer
-from verify_email.email_handler import send_verification_email
 
 
 class RegisterView(generics.CreateAPIView):
